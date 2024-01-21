19166c815f0cae817a06c3585751480fb2fcb7dc
ForeverAloners <tyzer1989@gmail.com>
2024-01-21 18:13:48 +0300

    fix: error to store,replace createStore to configureStore

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -2,9 +2,10 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import {charactersReducer} from "../reducers/characters-reducer/characters-reducer";
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import thunk from "redux-thunk"
+import {configureStore} from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({characters: charactersReducer})
-export const store = createStore(rootReducer, applyMiddleware(thunk))
+export const store = configureStore({reducer:rootReducer})
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
